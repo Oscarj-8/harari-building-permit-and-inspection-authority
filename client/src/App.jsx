63b5678fbefaa40ace0a6e775e8e793b96827727
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-09 13:57:07 +0300

    Add routes in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./components/pages/HomePage";
+import ServiceOne from "./components/pages/ServiceOne";
+import ServiceTwo from "./components/pages/ServiceTwo";
+import ServiceThree from "./components/pages/ServiceThree";
+import ServiceFour from "./components/pages/ServiceFour";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/service-one" element={<ServiceOne />} />
+        <Route path="/service-two" element={<ServiceTwo />} />
+        <Route path="/service-three" element={<ServiceThree />} />
+        <Route path="/service-four" element={<ServiceFour />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
