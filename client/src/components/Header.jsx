d01030656d38e26c5e1004ef577e94f185f54547
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-09 16:06:01 +0300

    Fix header and homepage styling issues

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className=" relative h-20 z-10 bg-slate-200 border p-5 shadow-md ">
+      <div className="flex justify-between items-center max-w-7xl mx-auto">
+        <span className="text-slate-700 text-lg">
+          <Link to="/">
+            <span className="text-3xl font-black">Harari </span>
+          </Link>
+          building permit and inspection authority
+        </span>
+        <ul className="flex gap-6">
+          <Link to="/service-one">
+            <li className="hover:underline">ServiceOne</li>
+          </Link>
+          <Link to="/service-two">
+            <li className="hover:underline">ServiceTwo</li>
+          </Link>
+          <Link to="/service-three">
+            <li className="hover:underline">ServiceThree</li>
+          </Link>
+          <Link to="/service-four">
+            <li className="hover:underline">ServiceFour</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
--- client/src/components/pages/HomePage.jsx
+++ client/src/components/pages/HomePage.jsx
@@ -1,3 +1,30 @@
+import background from "../../assets/images/Background.jpg";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const textStyle = {
+    fontFamily: "Montserrat, sans-serif",
+  };
+
+  return (
+    <main
+      className="flex items-center text-slate-700 p-5 "
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        height: "calc(100vh - 5rem)",
+      }}
+    >
+      <div className="absolute inset-0 bg-blue-950 opacity-40 filter blur-lg"></div>
+      <div className=" z-10 flex flex-col items-center max-w-6xl mx-auto gap-5 ">
+        <span className="text-4xl text-center text-white " style={textStyle}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+          consequatur molestias, tenetur unde repudiandae fugiat id cum deserunt
+          ut q
+        </span>
+        <button className="p-3 bg-blue-700 text-white rounded-lg shadow-lg border border-blue-700 hover:shadow-none hover:border hover:border-blue-700 hover:bg-white hover:text-blue-700 font-medium px-5 ">
+          Lets get Started
+        </button>
+      </div>
+    </main>
+  );
 }
