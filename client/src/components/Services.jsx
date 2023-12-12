236b2e0b5fa7d3d832f759822f671b9102bcde8e
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-12 15:50:36 +0300

    Add necessary section in Services page

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,3 +1,10 @@
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <main className="h-screen">
+      <section>Plan Consent</section>
+      <section>Design Evaluation and Building Permit</section>
+      <section>Building Inspection and Occupancy Permit</section>
+      <section>Construction regulatory</section>
+    </main>
+  );
 }
