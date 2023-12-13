753f6dc97eed09ee6ff9c8b99e63119790c9ff5e
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-13 13:38:32 +0300

    Start services section

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,90 +1,27 @@
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-import survey from "../assets/images/survey.png";
+import styled from "styled-components";
+import { services } from "../data/constants";
+
+const Card = styled.div``;
+Card.Header = styled.div``;
+Card.Description = styled.div``;
+Card.Bottom = styled.div``;
+
 export default function Services() {
   return (
-    <main className="flex flex-col gap-8 h-screen items-center">
+    <main className=" bg-slate-400 flex flex-col gap-8 items-center">
       <h1 className="text-3xl">Services</h1>
-      <div className="flex w-full items-strech justify-center gap-8">
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              className="w-32 "
-              image={survey}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Plan Consent
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Building Inspection and Occupancy Permit
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Building Inspection and Occupancy Permit
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Construction regulatory
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+      <div className="flex w-full flex-wrap items-strech justify-center gap-8">
+        {services.map((service) => (
+          <Card key={service.id}>
+            <Card.Header>
+              <img src={service.image} alt="service image" />
+            </Card.Header>
+            <Card.Description>
+              <p>{service.description}</p>
+            </Card.Description>
+            <Card.Bottom>{service.footer}</Card.Bottom>
+          </Card>
+        ))}
       </div>
     </main>
   );
