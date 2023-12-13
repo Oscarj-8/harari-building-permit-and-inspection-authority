cb81f81dc4fdbce6ef8c7f6889364208c8ae41f1
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-13 14:38:04 +0300

    Finish service page for now

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,26 +1,63 @@
 import styled from "styled-components";
 import { services } from "../data/constants";
+import { useEffect, useState } from "react";
 
 const Card = styled.div``;
 Card.Header = styled.div``;
 Card.Description = styled.div``;
 Card.Bottom = styled.div``;
 
+const AnimatedCard = styled(Card)`
+  opacity: 0;
+  transform: translateY(500px);
+  transition: opacity 3s ease-out, transform 3s ease-out;
+
+  &.fade-in {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
 export default function Services() {
+  const [isAnimated, setIsAnimated] = useState(false);
+
+  const handleScroll = () => {
+    const element = document.querySelector(".element");
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+      setIsAnimated(isVisible);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <main className=" bg-slate-400 flex flex-col gap-8 items-center">
-      <h1 className="text-3xl">Services</h1>
-      <div className="flex w-full flex-wrap items-strech justify-center gap-8">
-        {services.map((service) => (
-          <Card key={service.id}>
+    <main className={`flex flex-col gap-24 items-center `}>
+      <h1 className="text-5xl font-medium text-customBlue">Services</h1>
+      <div className="flex w-full flex-wrap items-stretch justify-center gap-8">
+        {services.map((service, index) => (
+          <AnimatedCard
+            key={service.id}
+            className={`${
+              isAnimated ? "fade-in" : ""
+            } flex flex-col justify-center items-center text-center w-full sm:w-3/4 md:w-1/4 rounded-lg p-4 gap-6 border border-slate-400 shadow-xl`}
+          >
             <Card.Header>
-              <img src={service.image} alt="service image" />
+              <img className="w-24" src={service.image} alt="service image" />
             </Card.Header>
-            <Card.Description>
+            <Card.Description className="text-slate-600">
               <p>{service.description}</p>
             </Card.Description>
-            <Card.Bottom>{service.footer}</Card.Bottom>
-          </Card>
+            <Card.Bottom className="rounded-full p-3 w-10 h-10 flex items-center justify-center bg-slate-200 text-customBlue">
+              {service.footer}
+            </Card.Bottom>
+          </AnimatedCard>
         ))}
       </div>
     </main>
--- client/src/components/pages/HomePage.jsx
+++ client/src/components/pages/HomePage.jsx
@@ -24,7 +24,7 @@ export default function HomePage() {
     setIsModalOpen(false);
   };
   return (
-    <div>
+    <div className="flex flex-col gap-16">
       <main
         className="flex items-center text-slate-700 p-5 "
         style={{
