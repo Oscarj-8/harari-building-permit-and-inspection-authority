331e08f4f208883d913d5875e281e699a05a4e8d
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-12-16 12:17:00 +0300

    Add google form to the service one

--- client/src/components/pages/ServiceOne.jsx
+++ client/src/components/pages/ServiceOne.jsx
@@ -5,9 +5,17 @@ export default function ServiceOne() {
   return (
     <div
       className="flex items-center justify-center bg-slate-400"
-      style={{ height: "calc(100vh - 5rem)" }}
+      // style={{ height: "calc(100vh - 5rem)" }}
     >
       <main>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLScX6M2901vmGQlFOJiUffYW1KhNjhf4vqIaTS5cjw2irxVwcA/viewform?embedded=true"
+          width="640"
+          height="957"
+          style={{ overflow: "hidden" }}
+        >
+          Loading…
+        </iframe>
         <Button variant="contained" className="bg-blue-700 ">
           <a download href={pdf}>
             Download
