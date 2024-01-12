ce1d703ca7ae2c916ff32da00a36769a038b3a2a
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-12 09:26:28 +0300

    Add description on service page

--- client/src/data/constants.js
+++ client/src/data/constants.js
@@ -5,28 +5,28 @@ export const services = [
     image: task,
     header: "Plan Consent",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. CulpaLorem ipsum dolor sit amet consectetur adipisicing elit. Culpa",
+      "Explore our streamlined Plan Consent service, guiding you through the approval of architectural and engineering plans. We ensure compliance with local regulations, zoning laws, and safety standards – the essential foundation for your construction project's legality.",
     footer: "1",
   },
   {
     image: task,
     header: "Design Evaluation and Building Permit",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. CulpaLorem ipsum dolor sit amet consectetur adipisicing elit. Culpa",
+      "Discover reliability in our Design Evaluation and Building Permit services. We meticulously examine plans, ensuring alignment with structural, safety, and aesthetic standards. Upon approval, our process swiftly grants you the necessary building permit.",
     footer: "2",
   },
   {
     image: task,
     header: "Building Inspection and Occupancy Permit",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. CulpaLorem ipsum dolor sit amet consectetur adipisicing elit. Culpa",
+      "Prioritize safety and quality with our Building Inspection and Occupancy Permit services. Experienced inspectors ensure strict adherence to approved plans and codes. Upon successful completion, receive an occupancy permit, allowing confident utilization of your newly constructed building.",
     footer: "3",
   },
   {
     image: task,
     header: "Construction Regulatory",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. CulpaLorem ipsum dolor sit amet consectetur adipisicing elit. Culpa",
+      "Navigate the construction industry's regulatory landscape effortlessly with our services. We guide you in upholding high standards in building materials, structural design, safety protocols, and environmental considerations. Your commitment ensures the legality, safety, and quality of your project.",
     footer: "4",
   },
 ];
