31ff7c66baca2b3ab338a57e548d4aa109089717
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-15 18:41:31 +0300

    Fix footer text issues

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="text-center bg-slate-200 p-2">
       <p>
-        &copy; 2024 Harari building permit and inspection authority. All Rights
+        &copy; 2024 Harari Building Permit and Inspection Authority. All Rights
         Reserved.
       </p>
     </div>
