afa1530eb85e6308618d71441d0a1c48538d2d70
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-17 13:47:49 +0300

    Add persisted storage to save admins infon local storage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { StyledEngineProvider } from "@mui/material";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
+    </PersistGate>
   </Provider>
 );
