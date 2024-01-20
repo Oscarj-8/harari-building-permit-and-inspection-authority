831a4f42fa443e35f0d6728290087631115c9d81
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-20 13:59:55 +0300

    Change unique folder generation method

--- api/routes/PlanConsentfileUpload.route.js
+++ api/routes/PlanConsentfileUpload.route.js
@@ -1,7 +1,48 @@
+// import express from "express";
+// import multer from "multer";
+// import fileUploadController from "../controllers/PlanConsentfileUpload.controller.js";
+// import ip from "ip";
+// import fs from "fs";
+// import path from "path";
+
+// const router = express.Router();
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     const userIdentifier = ip.address();
+//     const userFolder = path.join(
+//       process.cwd(),
+//       "api/planConsentFolder",
+//       userIdentifier
+//     );
+
+//     if (!fs.existsSync(userFolder)) {
+//       fs.mkdirSync(userFolder, { recursive: true });
+//     }
+
+//     cb(null, userFolder);
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     const fileName = `${uniqueSuffix}-${file.originalname}`;
+//     cb(null, fileName);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// router.post(
+//   "/uploadplanconsent",
+//   upload.fields([{ name: "file", maxCount: 1 }, { name: "scannedImages" }]),
+//   fileUploadController
+// );
+
+// export default router;
+
 import express from "express";
 import multer from "multer";
 import fileUploadController from "../controllers/PlanConsentfileUpload.controller.js";
-import ip from "ip";
+import { v4 as uuidv4 } from "uuid"; // Import UUID library
 import fs from "fs";
 import path from "path";
 
@@ -9,7 +50,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const userIdentifier = ip.address();
+    const userIdentifier = uuidv4(); // Generate a unique identifier for each user
     const userFolder = path.join(
       process.cwd(),
       "api/planConsentFolder",
