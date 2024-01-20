0513b0825a3ae80a7773a39b5b4b94c94de2eb56
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-20 15:17:28 +0300

    Test

--- api/routes/PlanConsentfileUpload.route.js
+++ api/routes/PlanConsentfileUpload.route.js
@@ -1,43 +1,43 @@
-// import express from "express";
-// import multer from "multer";
-// import fileUploadController from "../controllers/PlanConsentfileUpload.controller.js";
-// import ip from "ip";
-// import fs from "fs";
-// import path from "path";
+import express from "express";
+import multer from "multer";
+import fileUploadController from "../controllers/PlanConsentfileUpload.controller.js";
+import ip from "ip";
+import fs from "fs";
+import path from "path";
 
-// const router = express.Router();
+const router = express.Router();
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     const userIdentifier = ip.address();
-//     const userFolder = path.join(
-//       process.cwd(),
-//       "api/planConsentFolder",
-//       userIdentifier
-//     );
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const userIdentifier = ip.address();
+    const userFolder = path.join(
+      process.cwd(),
+      "api/planConsentFolder",
+      userIdentifier
+    );
 
-//     if (!fs.existsSync(userFolder)) {
-//       fs.mkdirSync(userFolder, { recursive: true });
-//     }
+    if (!fs.existsSync(userFolder)) {
+      fs.mkdirSync(userFolder, { recursive: true });
+    }
 
-//     cb(null, userFolder);
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     const fileName = `${uniqueSuffix}-${file.originalname}`;
-//     cb(null, fileName);
-//   },
-// });
+    cb(null, userFolder);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileName = `${uniqueSuffix}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
 
-// const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
-// router.post(
-//   "/uploadplanconsent",
-//   upload.fields([{ name: "file", maxCount: 1 }, { name: "scannedImages" }]),
-//   fileUploadController
-// );
+router.post(
+  "/uploadplanconsent",
+  upload.fields([{ name: "file", maxCount: 1 }, { name: "scannedImages" }]),
+  fileUploadController
+);
 
-// export default router;
+export default router;
 
 // import express from "express";
 // import multer from "multer";
@@ -81,45 +81,45 @@
 
 // export default router;
 
-import express from "express";
-import multer from "multer";
-import fileUploadController from "../controllers/PlanConsentfileUpload.controller.js";
-import { v4 as uuidv4 } from "uuid";
-import fs from "fs";
-import path from "path";
+// import express from "express";
+// import multer from "multer";
+// import fileUploadController from "../controllers/PlanConsentfileUpload.controller.js";
+// import { v4 as uuidv4 } from "uuid";
+// import fs from "fs";
+// import path from "path";
 
-const router = express.Router();
+// const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const userIdentifier =
-      req.session.userIdentifier + Math.round(Math.random() * 1e9) ||
-      (req.session.userIdentifier = uuidv4()); // Generate or retrieve from session
-    const userFolder = path.join(
-      process.cwd(),
-      "api/planConsentFolder",
-      userIdentifier
-    );
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     const userIdentifier =
+//       req.session.userIdentifier + Math.round(Math.random() * 1e9) ||
+//       (req.session.userIdentifier = uuidv4()); // Generate or retrieve from session
+//     const userFolder = path.join(
+//       process.cwd(),
+//       "api/planConsentFolder",
+//       userIdentifier
+//     );
 
-    if (!fs.existsSync(userFolder)) {
-      fs.mkdirSync(userFolder, { recursive: true }); // Create folder only if it doesn't exist
-    }
+//     if (!fs.existsSync(userFolder)) {
+//       fs.mkdirSync(userFolder, { recursive: true }); // Create folder only if it doesn't exist
+//     }
 
-    cb(null, userFolder);
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const fileName = `${uniqueSuffix}-${file.originalname}`;
-    cb(null, fileName);
-  },
-});
+//     cb(null, userFolder);
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     const fileName = `${uniqueSuffix}-${file.originalname}`;
+//     cb(null, fileName);
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
-router.post(
-  "/uploadplanconsent",
-  upload.fields([{ name: "file", maxCount: 1 }, { name: "scannedImages" }]),
-  fileUploadController
-);
+// router.post(
+//   "/uploadplanconsent",
+//   upload.fields([{ name: "file", maxCount: 1 }, { name: "scannedImages" }]),
+//   fileUploadController
+// );
 
-export default router;
+// export default router;
