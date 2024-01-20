f29aca5cb008b2322323bdc9fe3fb95aee6decc4
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-20 20:04:57 +0300

    Fix closing sidebar issue

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -140,6 +140,7 @@ export default function Header() {
               "
                     src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
                     alt="profile"
+                    onClick={toggleMenu}
                   />
                 </Link>
               )}
