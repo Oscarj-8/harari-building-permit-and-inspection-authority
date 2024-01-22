41fb21f3f9de06e5d049d6ae671ecb30acfe5c72
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-22 13:05:04 +0300

    Test 5

--- api/controllers/planConsent/PlanConsentfileGet.controller.js
+++ api/controllers/planConsent/PlanConsentfileGet.controller.js
@@ -6,17 +6,35 @@ import { errorHandler } from "../../utils/error.js";
 const getUserFolders = (req, res) => {
   const uploadsPath = path.join(process.cwd(), "api/planConsentFolder");
 
-  fs.readdir(uploadsPath, (err, folders) => {
-    if (folders || folders.length === 0) {
-      return res.status(200).json({ message: "No new folders or requests" });
-    } else if (err) {
-      return res
-        .status(500)
-        .json({ message: "Error fetching user folders", error: err });
+  // fs.readdir(uploadsPath, (err, folders) => {
+  //   if (folders || folders.length === 0) {
+  //     return res.status(200).json({ message: "No new folders or requests" });
+  //   } else if (err) {
+  //     return res
+  //       .status(500)
+  //       .json({ message: "Error fetching user folders", error: err });
+  //   } else {
+  //     res.status(200).json({ userFolders: folders });
+  //   }
+  // });
+  try {
+    const folders = fs.readdirSync(uploadsPath);
+
+    if (!folders || folders.length === 0) {
+      res.status(200).json({ message: "No new folders or requests" });
     } else {
       res.status(200).json({ userFolders: folders });
     }
-  });
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      res.status(404).json({ message: "User folder directory not found" });
+    } else {
+      // Handle other errors
+      res
+        .status(500)
+        .json({ message: "Error fetching user folders", error: error });
+    }
+  }
 };
 
 const downloadFolder = (req, res) => {
