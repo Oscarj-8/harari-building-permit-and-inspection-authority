fa6bb2688076ab4793d9e98ef7d9e6fd225d5906
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-22 12:38:28 +0300

    Test 3

--- api/controllers/planConsent/PlanConsentfileGet.controller.js
+++ api/controllers/planConsent/PlanConsentfileGet.controller.js
@@ -8,14 +8,14 @@ const getUserFolders = (req, res) => {
 
   fs.readdir(uploadsPath, (err, folders) => {
     if (err) {
-      return res.status(500).json({ message: "No new folders or requests" });
-    }
-
-    if (!folders || folders.length === 0) {
+      return res
+        .status(500)
+        .json({ message: "Error fetching user folders", error: err });
+    } else if (!folders || folders.length === 0) {
       return res.status(200).json({ message: "No new folders or requests" });
+    } else {
+      res.status(200).json({ userFolders: folders });
     }
-
-    res.status(200).json({ userFolders: folders });
   });
 };
 
