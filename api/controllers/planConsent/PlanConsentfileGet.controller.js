a2840d49e61ac619622ac2e2a46cd8b50b3d1953
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-24 12:08:52 +0300

    Remove comments lines and fix error fetching user folder error

--- api/controllers/planConsent/PlanConsentfileGet.controller.js
+++ api/controllers/planConsent/PlanConsentfileGet.controller.js
@@ -3,21 +3,39 @@ import fs from "fs";
 import archiver from "archiver";
 import { errorHandler } from "../../utils/error.js";
 
+// const getUserFolders = (req, res) => {
+//   const uploadsPath = path.join(process.cwd(), "api/planConsentFolder");
+
+//   try {
+//     const folders = fs.readdirSync(uploadsPath);
+
+//     if (!folders || (folders && folders.length === 0)) {
+//       res.status(200).json({ message: "No new folders or requests" });
+//     } else {
+//       res.status(200).json({ userFolders: folders });
+//     }
+//   } catch (error) {
+//     if (error.code === "ENOENT") {
+//       res.status(404).json({ message: "User folder directory not found" });
+//     } else {
+//       // Handle other errors
+//       res
+//         .status(500)
+//         .json({ message: "Error fetching user folders", error: error });
+//     }
+//   }
+// };
+
 const getUserFolders = (req, res) => {
   const uploadsPath = path.join(process.cwd(), "api/planConsentFolder");
 
-  // fs.readdir(uploadsPath, (err, folders) => {
-  //   if (folders || folders.length === 0) {
-  //     return res.status(200).json({ message: "No new folders or requests" });
-  //   } else if (err) {
-  //     return res
-  //       .status(500)
-  //       .json({ message: "Error fetching user folders", error: err });
-  //   } else {
-  //     res.status(200).json({ userFolders: folders });
-  //   }
-  // });
   try {
+    // Check if the directory exists
+    if (!fs.existsSync(uploadsPath)) {
+      res.status(200).json({ message: "No new folders or requests" });
+      return;
+    }
+
     const folders = fs.readdirSync(uploadsPath);
 
     if (!folders || (folders && folders.length === 0)) {
