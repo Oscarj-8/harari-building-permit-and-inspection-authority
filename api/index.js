5e0f62077ddebda875a9cebb719e91c85b98ff10
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-27 13:46:58 +0300

    Handle routing on the front end

--- api/index.js
+++ api/index.js
@@ -57,6 +57,10 @@ app.use(
 //   );
 // });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
