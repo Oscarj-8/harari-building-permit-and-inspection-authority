7d9dbd9ccce9d5c37991c4871a2a93a2af2100b0
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-01-29 00:11:16 +0300

    Set session exp in 1 min

--- api/controllers/user/userAuth.controller.js
+++ api/controllers/user/userAuth.controller.js
@@ -24,13 +24,15 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1m",
+    });
     const { password: pass, ...rest } = validUser._doc;
 
     res
       .cookie("access_token", token, {
         httpOnly: true,
-        expires: 0,
+        expires: new Date(Date.now() + 60000),
       })
       .status(200)
       .json(rest);
