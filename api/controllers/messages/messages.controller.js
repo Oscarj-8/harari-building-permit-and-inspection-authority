7e9db55bbae0e167642a37af4829c65c28d04d6b
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-02-01 15:43:41 +0300

    Add comments list page and start creating route for getting the messages

--- api/controllers/messages/messages.controller.js
+++ api/controllers/messages/messages.controller.js
@@ -1,3 +1,35 @@
+// import fs from "fs";
+// import path from "path";
+
+// const currentDir = new URL(".", import.meta.url).pathname;
+// const MESSAGES_FOLDER = path.join(currentDir, "..", "..", "messagesFolder");
+
+// const handleMessages = (req, res) => {
+//   try {
+//     const { name, number, message } = req.body;
+
+//     const timestamp = Date.now();
+
+//     const messageFolder = path.join(MESSAGES_FOLDER, String(timestamp));
+//     fs.mkdirSync(messageFolder, { recursive: true });
+
+//     const messageFilePath = path.join(messageFolder, "message.txt");
+//     fs.writeFileSync(
+//       messageFilePath,
+//       `Name: ${name}\nNumber: ${number}\nMessage: ${message}`
+//     );
+
+//     res
+//       .status(200)
+//       .json({ success: true, message: "Message saved successfully" });
+//   } catch (error) {
+//     console.error("Error saving message:", error);
+//     res.status(500).json({ success: false, error: "Failed to save message" });
+//   }
+// };
+
+// export default handleMessages;
+
 import fs from "fs";
 import path from "path";
 
@@ -8,12 +40,11 @@ const handleMessages = (req, res) => {
   try {
     const { name, number, message } = req.body;
 
-    const timestamp = Date.now();
+    const messageFilePath = path.join(
+      MESSAGES_FOLDER,
+      `message_${Date.now()}.txt`
+    );
 
-    const messageFolder = path.join(MESSAGES_FOLDER, String(timestamp));
-    fs.mkdirSync(messageFolder, { recursive: true });
-
-    const messageFilePath = path.join(messageFolder, "message.txt");
     fs.writeFileSync(
       messageFilePath,
       `Name: ${name}\nNumber: ${number}\nMessage: ${message}`
