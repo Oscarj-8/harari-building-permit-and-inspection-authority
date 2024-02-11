7ca9c5e01d1c2b26e2a38ddc9103597da5f1c5f1
Oscar Junior <mmmabdulahi954@yahoo.com>
2024-02-11 15:30:45 +0300

    Fix width issue

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,9 +8,9 @@ export default function About() {
           <h1 className=" font-medium text-customBlue text-3xl lg:text-4xl">
             About Us
           </h1>
-          <p className="text-slate-600">{about.text}</p>
+          <p className="text-slate-600 max-w-5xl">{about.text}</p>
         </div>
-        <div className=" flex-1 max-w-lg flex items-center justify-end">
+        <div className=" flex-1 max-w-md flex items-center justify-end">
           <img
             className="rounded-md w-full"
             src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YWJvdXQlMjB1c3xlbnwwfHwwfHx8MA%3D%3D"
