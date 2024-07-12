f41d1b9af0a02000d10c59067d54bac5e6643c01
Abdulahi <mmmabdulahi954@yahoo.com>
2024-07-12 14:37:13 +0300

    Mkae email not required

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -7,11 +7,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    // email: {
-    //   type: String,
-    //   required: true,
-    //   unique: true,
-    // },
+    email: {
+      type: String,
+      required: false,
+      unique: false,
+    },
     phoneNumber: {
       type: Number,
       required: true,
